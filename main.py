932857f62ed50ff80c8469d3ea4b846642c3b268
Mohammad <mohammad.nazari@live.com>
2025-11-22 20:41:28 +0330

    feat: Implement user listing and duplicate email validation

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
-from fastapi import FastAPI, status
+from fastapi import FastAPI, status, HTTPException
 from schemas import UserRequest, UserResponse, UserOutput
+from typing import List
 
 app = FastAPI()
 
@@ -12,9 +13,20 @@ def read_root():
 @app.post("/user", response_model=UserResponse, status_code=status.HTTP_201_CREATED)
 def create_user(user: UserRequest):
     # print(user.model_dump())
+
+    if any(u.email == user.email for u in users):
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already exists"
+        )
+
     new_user = UserOutput(
         id=len(users) + 1,
         **user.model_dump()
     )
     users.append(new_user)
     return {"message": "User created", "user": new_user}
+
+@app.get("/user", response_model=List[UserOutput])
+def get_users():
+    return users
